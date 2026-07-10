bd250bebd35ccc9ce0b4a247862700b4b803aea3
mario4 <mariopaulosoares@gmail.com>
2026-07-10 22:37:00 +0200

    refactor ErrorResponse to record

--- app/src/main/java/devices/adapter/in/ErrorResponse.java
+++ app/src/main/java/devices/adapter/in/ErrorResponse.java
@@ -1,26 +1,4 @@
 package devices.adapter.in;
 
-public class ErrorResponse {
-
-    private int status;
-    private String error;
-    private String message;
-
-    public ErrorResponse() {}
-
-    public ErrorResponse(int status, String error, String message) {
-        this.status = status;
-        this.error = error;
-        this.message = message;
-    }
-
-    // getters and setters
-    public int getStatus() { return status; }
-    public void setStatus(int status) { this.status = status; }
-
-    public String getError() { return error; }
-    public void setError(String error) { this.error = error; }
-
-    public String getMessage() { return message; }
-    public void setMessage(String message) { this.message = message; }
+public record ErrorResponse (int status, String error, String message){
 }
