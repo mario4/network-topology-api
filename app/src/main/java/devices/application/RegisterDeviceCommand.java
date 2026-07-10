e9fdac34fed69a0c53fe74c2ae94eeeb63ad2b7d
mario4 <mariopaulosoares@gmail.com>
2026-07-10 22:42:41 +0200

    separate register device use case from domain logic

---
+++ app/src/main/java/devices/application/RegisterDeviceCommand.java
@@ -0,0 +1,11 @@
+package devices.application;
+
+import devices.model.DeviceType;
+import devices.model.MacAddress;
+
+public class RegisterDeviceCommand {
+
+    private DeviceType deviceType;
+
+    private MacAddress macAddress;
+}
---
+++ app/src/main/java/devices/application/RegisterDeviceUseCase.java
@@ -0,0 +1,12 @@
+package devices.application;
+
+import devices.port.out.DevicesNetworkRepository;
+
+public class RegisterDeviceUseCase {
+
+    private DevicesNetworkRepository networkRepository;
+
+    public void execute(RegisterDeviceCommand command){
+
+    }
+}
